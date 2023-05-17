0570ec39f3454248d68b37941bcd9b21ea82a08f
Mariya <130490657+MariaK1007@users.noreply.github.com>
2023-05-17 23:15:35 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# Адрес приложения url
+URL_SERVICE = "https://e31121df-9e32-4b57-91be-50b9e2d30b0e.serverhub.praktikum-services.ru"
+
+# Эдпойнт для создания заказа
+CREATE_ORDERS = "/api/v1/orders"
+
+# Эдпойт для получения заказа по его номеру
+CREATE_ORDERS_TRACK = '/api/v1/orders/track?t='
---
+++ data.py
@@ -0,0 +1,16 @@
+# Тело запроса для создания заказа
+orders_body = {
+    "firstName": "Maria",
+    "lastName": "Kopaeva",
+    "address": "Pervomayskaya, 82 apt.",
+    "metroStation": 8,
+    "phone": "+7 999 888 77 66",
+    "rentTime": 15,
+    "deliveryDate": "2023-05-05",
+    "comment": "Saske, come back to home",
+    "color": [
+        "BLACK"
+    ]
+}
+
+
